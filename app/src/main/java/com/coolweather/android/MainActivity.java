4c49f015c8e859ea98e5bc48bb14151700672550
张万森 <571943132@qq.com>
2023-02-03 15:09:05 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -3,7 +3,12 @@ package com.coolweather.android;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather_id",null) !=null){
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
